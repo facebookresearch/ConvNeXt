2bc3bc0cbd660d72bfcb48bc9c02e1830ce4ee2c
hmichaeli <54476245+hmichaeli@users.noreply.github.com>
2022-04-20 16:08:57 +0300

    bugfix - cosine scheduler with warmup steps
    Fix assertion when warmup_epochs = 0, warmup_steps > 0

--- utils.py
+++ utils.py
@@ -432,7 +432,7 @@ def cosine_scheduler(base_value, final_value, epochs, niter_per_ep, warmup_epoch
     if warmup_steps > 0:
         warmup_iters = warmup_steps
     print("Set warmup steps = %d" % warmup_iters)
-    if warmup_epochs > 0:
+    if warmup_iters > 0:
         warmup_schedule = np.linspace(start_warmup_value, base_value, warmup_iters)
 
     iters = np.arange(epochs * niter_per_ep - warmup_iters)
